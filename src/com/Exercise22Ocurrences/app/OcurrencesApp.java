f7d893b33e3eb89877bced02135746ef5f3c8e37
Mabestron <ivangamaliel555@hotmail.com>
2019-10-23 13:58:37 -0500

    Ocurrences v2

--- src/com/Exercise22Ocurrences/app/OcurrencesApp.java
+++ src/com/Exercise22Ocurrences/app/OcurrencesApp.java
@@ -15,17 +15,52 @@ public class OcurrencesApp
 		Random numbers = new Random(System.nanoTime());
 		HashMap <Integer, Integer> miHash =new HashMap<Integer,Integer>();
 
-		int[] miArray = new int [ELEMENTS];
+		int[] miArray = new int [ELEMENTS];// se crea un arreglo de enteros
+		
+		
+		//se rellena el vector
 		for (int i=0;i<ELEMENTS;i++)
 		{
 			miArray[i] =numbers.nextInt(101);
 		}
 		
+		//se muestra el vector con un for each
+		// por cada elemento "i" que se encuentra dentro del arreglo "miArray", se imprime el elemento
 		for (int i: miArray)
 		{
 			System.out.print(" "+i);
 		}
 		
+		//process
+		int ocurrences=0;
+		for (int element: miArray)
+		{
+			
+			if (miHash.containsKey(element))
+			{
+				ocurrences=miHash.get(element);
+				ocurrences++;
+				miHash.put(element, ocurrences);
+				
+			}
+			else 
+			{
+				miHash.put(element, 1);
+			}
+			
+			//miHash.putIfAbsent(element, 1);  //Alternativo del else
+		}
+		
+	System.out.println(" ");	
+	System.out.println(miHash);	
+	
+	/*
+	for (int i: miArray)
+	{
+		System.out.print(" "+i);
+	}
+	*/
+	
 	}
 
 }
